60ffbf22547bda7028098c702eb34dfbe769161e
Rex2002 <frahm.benjamin@gmx.de>
2023-05-05 12:15:05 +0200

    feat: add kick button for mod, add server endpoint for player.kick

--- frontend/src/components/ListPlayer.js
+++ frontend/src/components/ListPlayer.js
@@ -1,14 +1,16 @@
-import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material"
+import { List, ListItem, ListItemIcon, ListItemText, Button } from "@mui/material"
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import StarIcon from '@mui/icons-material/Star';
 
-const ListPlayer = ({ player }) => {
+const ListPlayer = ({ player, kickHandler }) => {
+    console.log("kick handler: " + kickHandler)
     return (
         <List>
             {player.map(p =>
                 <ListItem key={p.name}>
                     {p.isBoss ? <ListItemIcon> <StarIcon /></ListItemIcon> : <ListItemIcon><AccountBoxIcon /> </ListItemIcon>}
                     <ListItemText primary={p.name + (p.isMod ? " (MOD)" : "")} secondary={"Points:" + p.points} />
+                    {kickHandler !== undefined && <Button color="error" onClick={() => kickHandler(p.name)}>Kick</Button>}
                 </ListItem>
             )}
         </List>
